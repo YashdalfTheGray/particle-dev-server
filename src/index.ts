aecdcccc79013a442a97cc2236655dd629bb8597
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-01 04:53:11 +0530

    wired in the new route to the server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import * as express from 'express';
 import * as morgan from 'morgan';
 import * as chalk from 'chalk';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+import { getDevicesRoot } from './routes/devices';
 
 const app = express();
 const appPort = process.env.PORT || process.argv[2] || 8080;
 
-app.get('*', (req, res) => {
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
     res.json({
-        status: 'ok'
+        status: 'ok',
+        configFileFound: !!process.env.CONFIG_FILE
     });
 });
+
+app.get('/devices', getDevicesRoot);
 
 app.listen(appPort, () => {
     console.log(`Server is now listening at port ${chalk.green(appPort)}...`)
