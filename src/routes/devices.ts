69f74e61cafcf2834576afc4c451e52d8d0ae24f
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-04 11:58:55 -0500

    the devices matcher now returns the contents of the config file

--- src/routes/devices.ts
+++ src/routes/devices.ts
@@ -2,9 +2,14 @@ import { Request, Response, NextFunction } from 'express';
 
 const getDevices = (req: Request, res: Response, next: NextFunction) => {
     if (/^\/devices\//.test(req.path)) {
-        res.json({
-            requestedPath: req.path
-        });
+        if (!!process.env.CONFIG_FILE_PATH) {
+            res.json(require(process.cwd() + '/' + process.env.CONFIG_FILE_PATH));
+        }
+        else {
+            res.json({
+                requestedPath: req.path
+            });
+        }
     }
     else {
         next();
