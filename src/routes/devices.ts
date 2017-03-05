4d52b7d816873a67989d4f7e39320ab3abe18f7d
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-03-05 14:23:21 -0800

    pulled the returning json into another function

--- src/routes/devices.ts
+++ src/routes/devices.ts
@@ -1,9 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 
-const getDevices = (req: Request, res: Response, next: NextFunction) => {
+const getDevices = () => {
+    return require(process.cwd() + '/' + process.env.CONFIG_FILE_PATH)
+}
+
+const handleDeviceRequest = (req: Request, res: Response, next: NextFunction) => {
     if (/^\/devices\//.test(req.path)) {
         if (!!process.env.CONFIG_FILE_PATH) {
-            res.json(require(process.cwd() + '/' + process.env.CONFIG_FILE_PATH));
+            res.json(getDevices());
         }
         else {
             res.json({
@@ -17,5 +21,5 @@ const getDevices = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export {
-    getDevices
+    handleDeviceRequest
 };
