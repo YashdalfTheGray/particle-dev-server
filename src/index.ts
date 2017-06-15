2ce1f49e0ebf4b124ea25da845c193aa264fd00f
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-15 10:11:55 -0800

    mass file culling happening right now

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as chalk from 'chalk';
 
-import { handleDeviceRequest } from './routes/devices';
 import { getStatus } from './routes/status';
 
 const app = express();
 const appPort = process.env.PORT || process.argv[2] || 8080;
 
 app.use(morgan('dev'));
-app.use(handleDeviceRequest);
 
 app.get('/', getStatus);
 
