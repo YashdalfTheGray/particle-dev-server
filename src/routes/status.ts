6793137e403392c4f4c73ceb446cce4f2cd57ce8
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-15 10:11:19 -0800

    changed up the status

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 const getStatus = (req: Request, res: Response) => {
     res.json({
         status: 'ok',
-        configFileFound: !!process.env.CONFIG_FILE_PATH
+        devicesFile: !!process.argv[2]
     });
 };
 
