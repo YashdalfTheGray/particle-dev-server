e6fee9efa3057319628401a01049b7334916ae95
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-16 09:44:11 -0800

    added the hashbang and wired up auth and stuff

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
+#!/usr/bin/env node
+
 import * as express from 'express';
 import * as morgan from 'morgan';
 import * as chalk from 'chalk';
 
 import { getStatus } from './routes/status';
+import { authenticate } from './routes/auth';
 
 const app = express();
 const appPort = process.env.PORT || process.argv[2] || 8080;
+const appRouter = express.Router();
 
 app.use(morgan('dev'));
 
+appRouter.get('/devices', authenticate, (req, res) => res.json({ foo: 'test', bar: true }));
+
 app.get('/', getStatus);
+app.use('/v1', appRouter);
 
 app.listen(appPort, () => {
     console.log(`Server is now listening at port ${chalk.green(appPort)}...`)
