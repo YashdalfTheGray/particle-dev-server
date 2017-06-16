d0ffad6015025500e0cdf401c9605cb15f6f690c
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-16 09:43:14 -0800

    added an interface for the API config object

---
+++ src/ApiConfig.ts
@@ -0,0 +1,25 @@
+interface Device {
+    id: string;
+    name: string;
+    last_app: string;
+    connected: string;
+    variables?: Object;
+    functions?: string[];
+    cc3000_patch_version?: string;
+    product_id: number;
+    last_heard: Date;
+    requires_deep_update?: boolean;
+    last_iccid?: string;
+    imei?: string;
+}
+
+interface ApiConfig {
+    token: string;
+    devices: Device[];
+}
+
+export default ApiConfig;
+
+export {
+    Device
+};
