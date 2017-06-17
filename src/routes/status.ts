f5958ff5716fee926b8da1206f9c59a120263eef
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-17 00:24:48 -0800

    fixed the file found name

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 const getStatus = (req: Request, res: Response) => {
     res.json({
         status: 'ok',
-        devicesFile: !!process.argv[2]
+        devicesFileFound: !!process.argv[2]
     });
 };
 
