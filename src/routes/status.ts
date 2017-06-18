5ac19c9f1939f60ca7a5ada8d8c549d16f4a14b3
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-18 11:57:07 -0700

    file should be in process.argv[1] instead of argv[2]

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 const getStatus = (req: Request, res: Response) => {
     res.json({
         status: 'ok',
-        devicesFileFound: !!process.argv[2]
+        devicesFileFound: !!process.argv[1]
     });
 };
 
