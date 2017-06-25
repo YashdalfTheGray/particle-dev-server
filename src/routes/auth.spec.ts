0ec613084d96fc4b656174e4bfe53b2a03a46557
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-24 17:09:45 -0700

    handling more test cases

--- src/routes/auth.spec.ts
+++ src/routes/auth.spec.ts
@@ -3,6 +3,19 @@ import { Request } from 'express';
 
 import { findToken } from './auth';
 
+test('throws missing token error when there is no token', t => {
+    const request = {
+        get: (name: string) => null
+    };
+
+    const foundToken = findToken(request as Request);
+
+    t.deepEqual(foundToken, {
+        error: 'invalid_request',
+        error_description: 'the access token was not found'
+    });
+});
+
 test('findToken finds the right token when in the header', t => {
     const request = {
         get: (name: string) => 'Bearer test-auth-token'
@@ -11,6 +24,32 @@ test('findToken finds the right token when in the header', t => {
     const foundToken = findToken(request as Request);
 
     t.is(foundToken, 'test-auth-token');
+});
+
+test('findToken errors when there is no token', t => {
+    const request = {
+        get: (name: string) => null
+    };
+
+    const foundToken = findToken(request as Request);
+
+    t.deepEqual(foundToken, {
+        error: 'invalid_request',
+        error_description: 'the access token was not found'
+    });
+});
+
+test('findToken errors when the token type is not set', t => {
+    const request = {
+        get: (name: string) => 'test-auth-token'
+    };
+
+    const foundToken = findToken(request as Request);
+
+    t.deepEqual(foundToken, {
+        error: 'invalid_request',
+        error_description: 'malformed auth header'
+    });
 });
 
 test('findToken finds the right token when added as a query on GET', t => {
