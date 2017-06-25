7116b36bd928fe656624e14bf344d81b01a2c1bd
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-06-24 17:05:11 -0700

    added tests for all the successful cases

--- src/routes/auth.spec.ts
+++ src/routes/auth.spec.ts
@@ -12,3 +12,31 @@ test('findToken finds the right token when in the header', t => {
 
     t.is(foundToken, 'test-auth-token');
 });
+
+test('findToken finds the right token when added as a query on GET', t => {
+    const request = {
+        get: (name: string) => null,
+        method: 'GET',
+        query: {
+            access_token: 'test-auth-token'
+        }
+    };
+
+    const foundToken = findToken(request as Request);
+
+    t.is(foundToken, 'test-auth-token');
+});
+
+test('findToken finds the right token when added as in the POST body', t => {
+    const request = {
+        get: (name: string) => name === 'Content-Type' ? 'application/x-www-form-urlencoded' : null,
+        method: 'POST',
+        body: {
+            access_token: 'test-auth-token'
+        }
+    };
+
+    const foundToken = findToken(request as Request);
+
+    t.is(foundToken, 'test-auth-token');
+});
